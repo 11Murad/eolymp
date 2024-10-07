2caa5ce8d65fdd41d9945054b42763378eb45d3f
Murad <ashrafov.main@gmail.com>
2024-10-07 17:37:33 +0400

    Optimallasdirildi

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-
-
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        if ((a > 0 && b > 0 && c < 0) || (a > 0 && b < 0 && c > 0) || (a < 0 && b > 0 && c > 0)
+        || (a < 0 && b < 0 && c > 0) || (a > 0 && b < 0 && c < 0)){
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
     }
 }
