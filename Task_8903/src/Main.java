1b850aa6a2849e9921c770265bdc83bf6e6f6d88
Murad <ashrafov.main@gmail.com>
2024-10-10 15:37:20 +0400

    arrays added

--- Task_8903/src/Main.java
+++ Task_8903/src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        while (true) {
-            n = n - 1;
-            if (n % 5 == 0) {
-                System.out.println(n);
-                break;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (j > 0 && j < 5) {
+                    System.out.print(" ");
+                }
+                else {
+                    System.out.print("*");}
             }
         }
     }
